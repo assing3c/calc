858c5382c0144cb1bd3efdb27fae96cd8a0a2d4e
fafik <assing3c@gmail.com>
2026-09-14 22:34:49 +0500

    Update Program.cs

--- calc/Program.cs
+++ calc/Program.cs
@@ -1,10 +1,10 @@
 ﻿using System.Diagnostics.CodeAnalysis;
-Console.WriteLine("Введите число 1:");
+Console.WriteLine("Введите первое число:");
 int num1 = int.Parse(Console.ReadLine());
-Console.WriteLine("Введите число 2:");
+Console.WriteLine("Введите второе число:");
 int num2 = int.Parse(Console.ReadLine());
 
-Console.WriteLine("Выбиерите функцию: +, -, *, / ");
+Console.WriteLine("Введите действие");
 char operation = Convert.ToChar(Console.ReadLine());
 
 switch (operation)
