2fb9c48163d2c480e2d7ddedfd8efcbbfc7c790c
-rxy- <-rxy-@DESKTOP-SAVMEP4>
2026-09-14 21:57:47 +0500

    Добавьте файлы проекта.

---
+++ calc/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics.CodeAnalysis;
+Console.WriteLine("Введите число 1:");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число 2:");
+int num2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Выбиерите функцию: +, -, *, / ");
+char operation = Convert.ToChar(Console.ReadLine());
+
+switch (operation)
+{
+    case '+':
+        Console.WriteLine(num1 + num2);
+        break;
+
+    case '-':
+        Console.WriteLine(num1 - num2);
+        break;
+
+    case '*':
+        Console.WriteLine(num1 * num2);
+        break;
+
+    case '/':
+        Console.WriteLine(num1 / num2);
+        break;
+
+    default:
+        Console.WriteLine("Вы ввели неправильный знак, повторите попытку");
+        break;
+
+}
